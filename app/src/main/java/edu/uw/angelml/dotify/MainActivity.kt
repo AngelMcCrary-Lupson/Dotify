9258bdd2a2b8c952224a6caed345afc4a5919c2a
AngelMcCrary-Lupson <angel.mccrary.lupson@gmail.com>
2021-04-11 17:16:19 -0700

    Added user name button functionality, cleaned up code, added custom song, extracted fields to value files
    Added user name button functionality, cleaned up code, added custom song, extracted fields to value files

--- app/src/main/java/edu/uw/angelml/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/angelml/dotify/MainActivity.kt
@@ -2,9 +2,7 @@ package edu.uw.angelml.dotify
 
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -14,15 +12,39 @@ class MainActivity : AppCompatActivity() {
     private val randPlayCount: Int = (10000 - (Math.random() * 10000)).toInt()
     private lateinit var playCountView: TextView
     private var currPlayCount: Int = randPlayCount
+    private var userNameChange: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Set Up Change User Name Button
+        val changeUserBtn = findViewById<Button>(R.id.changeUserBtn)
+        var userNameTextView = findViewById<TextView>(R.id.userNameTextView)
+        var userNameEditText = findViewById<EditText>(R.id.userNameInput)
+        userNameEditText.visibility = EditText.GONE
+        changeUserBtn.setOnClickListener {
+            if (!userNameChange) { // User name is text view
+                userNameTextView.visibility = TextView.GONE
+                userNameEditText.visibility = EditText.VISIBLE
+                changeUserBtn.text = getString(R.string.apply_user)
+                userNameChange = !userNameChange
+            } else {
+                val userInput = userNameEditText.text
+                userNameTextView.text = userInput
+                userNameTextView.visibility = TextView.VISIBLE
+                userNameEditText.visibility = EditText.GONE
+                changeUserBtn.text = getString(R.string.change_user)
+                userNameChange = !userNameChange
+            }
+        }
+
+        // Make Play Count a Random Number
         playCountView = findViewById<TextView>(R.id.playCountView)
-        val playCountText = randPlayCount.toString() + " plays"
+        val playCountText = randPlayCount.toString() + " " + getString(R.string.plays)
         playCountView.text = playCountText
 
+        // Set Up Playback Buttons
         val playBtn = findViewById<ImageView>(R.id.playBtn)
         playBtn.setOnClickListener{ playBtnPress() }
 
@@ -32,10 +54,9 @@ class MainActivity : AppCompatActivity() {
         val forwardBtn = findViewById<ImageView>(R.id.forwardBtn)
         forwardBtn.setOnClickListener { forwardBtnPress() }
 
-        // Extra Credit
+        // Extra Credit - Change Play Count Color
         val songCoverImg = findViewById<ImageView>(R.id.songCoverImg)
         songCoverImg.setOnLongClickListener{
-            // Toast.makeText(this, "Long click detected", Toast.LENGTH_SHORT).show()
             changePlayCountColor()
             true
         }
@@ -49,16 +70,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun prevBtnPress () {
-        Toast.makeText(this, "Skipping to previous track", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.skip_prev), Toast.LENGTH_SHORT).show()
     }
 
     fun forwardBtnPress () {
-        Toast.makeText(this, "Skipping to next track", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, getString(R.string.skip_next), Toast.LENGTH_SHORT).show()
     }
 
     fun playBtnPress () {
         currPlayCount++
-        val newPlayCountText = currPlayCount.toString() + " plays"
+        val newPlayCountText = currPlayCount.toString() + " " + getString(R.string.plays)
         playCountView.text = newPlayCountText
     }
 }
