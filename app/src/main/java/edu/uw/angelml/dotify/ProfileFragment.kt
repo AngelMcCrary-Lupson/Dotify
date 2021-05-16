a0268d3a83979dadca8bc7b98be1f20042b4e955
AngelMcCrary-Lupson <angel.mccrary.lupson@gmail.com>
2021-05-16 11:22:07 -0700

    added better error handling + app screenshots

--- app/src/main/java/edu/uw/angelml/dotify/ProfileFragment.kt
+++ app/src/main/java/edu/uw/angelml/dotify/ProfileFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.lifecycle.lifecycleScope
 import coil.load
 import edu.uw.angelml.dotify.databinding.FragmentProfileBinding
@@ -32,12 +33,13 @@ class ProfileFragment : Fragment() {
                     email.text = user.firstName + user.lastName + "@hogwarts.edu"
                     // Use Coil to load the Profile Image
                     profilePic.load(user.profilePicURL)
-
+                    // Hide Error
+                    profileError.visibility = LinearLayout.GONE
                     // Shows number of songs listened to during the app session from SongManager
                     val songsPlayed = dotifyApplication.songManager.getSongsPlayed()
                     songsListened.text = "Total Songs Played This Session: " + songsPlayed
                 }.onFailure {
-                    username.text = "An error occured when fetching your profile"
+                    profileError.visibility = LinearLayout.VISIBLE
                 }
             }
         }
